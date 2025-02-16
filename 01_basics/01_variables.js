9081642f7d681e1680324cfc303d621761db02bd
Krish Birla <144705679+shrikrishbirla@users.noreply.github.com>
2025-02-16 20:03:10 +0000

    concept of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 23145;
+let accountEmail = "krishbirla19@gmail.com";
+var accountPassword = "1234567";
+accountCity = "Chandigarh";
+
+// accountId = 22 --> throws error as value can't change and remains same as assigned before
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
