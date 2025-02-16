671a452bb8e4cab9d681ea4e06659334edf0ed92
Krish Birla <144705679+shrikrishbirla@users.noreply.github.com>
2025-02-16 20:19:46 +0000

    concept of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert() --> we are using nodejs, not browser | throws error
+
+console.log("Hello, World");
+
+console.log("Learn JS with ME");
+
+// work on code readability
+
+let name = "Krish"
+let age = 21
+let isLoggedIn = false
+
+// number --> 2 to power 53
+// bigInt
+// string --> "", ''
+// boolean --> true/false
+// null --> (standalone value)
+// undefined --> (value not assigned)
+// symbol --> (unique)
+
+// object
+
+
+console.log(typeof null); // --> output will be object
+console.log(typeof undefined); // --> output will be undefined
+
+
